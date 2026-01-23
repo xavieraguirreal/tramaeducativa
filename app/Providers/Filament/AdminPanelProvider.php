39dcda511efa6140df5ee3a7b21cc651bf87bf74
Xavier Aguirre <xagustinijms@gmail.com>
2026-01-23 08:04:42 -0300

    Cambiar ruta panel a /te2025

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('gestion')
+            ->path('te2025')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
